05b1d5b19c8920b3c9d7617f2a773f96db8539fa
Anto Subash <antosubash@live.com>
2026-03-18 09:42:52 +0100

    test: add targeted tests for individual emitters

---
+++ tests/SimpleModule.Generator.Tests/EndpointExtensionsEmitterTests.cs
@@ -0,0 +1,339 @@
+using FluentAssertions;
+using SimpleModule.Generator.Tests.Helpers;
+
+namespace SimpleModule.Generator.Tests;
+
+public class EndpointExtensionsEmitterTests
+{
+    [Fact]
+    public void Module_WithRoutePrefix_CreatesMapGroupWithTags()
+    {
+        var source = """
+            using Microsoft.AspNetCore.Builder;
+            using Microsoft.AspNetCore.Routing;
+            using SimpleModule.Core;
+
+            namespace TestApp
+            {
+                [Module("Products", RoutePrefix = "/api/products")]
+                public class ProductsModule : IModule { }
+            }
+
+            namespace TestApp.Endpoints
+            {
+                public class ListEndpoint : IEndpoint
+                {
+                    public void Map(IEndpointRouteBuilder app)
+                    {
+                        app.MapGet("/", () => "list");
+                    }
+                }
+            }
+            """;
+
+        var compilation = GeneratorTestHelper.CreateCompilation(source);
+        var result = GeneratorTestHelper.RunGenerator(compilation);
+
+        var endpointExt = GetGeneratedSource(result, "EndpointExtensions.g.cs");
+
+        endpointExt.Should().Contain("var group = app.MapGroup(\"/api/products\").WithTags(\"Products\");");
+        endpointExt.Should().Contain("new global::TestApp.Endpoints.ListEndpoint().Map(group);");
+    }
+
+    [Fact]
+    public void Module_WithoutRoutePrefix_MapsEndpointsDirectlyToApp()
+    {
+        var source = """
+            using Microsoft.AspNetCore.Builder;
+            using Microsoft.AspNetCore.Routing;
+            using SimpleModule.Core;
+
+            namespace TestApp
+            {
+                [Module("Misc")]
+                public class MiscModule : IModule { }
+            }
+
+            namespace TestApp.Endpoints
+            {
+                public class PingEndpoint : IEndpoint
+                {
+                    public void Map(IEndpointRouteBuilder app)
+                    {
+                        app.MapGet("/ping", () => "pong");
+                    }
+                }
+            }
+            """;
+
+        var compilation = GeneratorTestHelper.CreateCompilation(source);
+        var result = GeneratorTestHelper.RunGenerator(compilation);
+
+        var endpointExt = GetGeneratedSource(result, "EndpointExtensions.g.cs");
+
+        endpointExt.Should().Contain("new global::TestApp.Endpoints.PingEndpoint().Map(app);");
+        endpointExt.Should().NotContain("MapGroup");
+    }
+
+    [Fact]
+    public void Module_WithConfigureEndpoints_SkipsAutoRegistration()
+    {
+        var source = """
+            using Microsoft.AspNetCore.Builder;
+            using Microsoft.AspNetCore.Routing;
+            using SimpleModule.Core;
+
+            namespace TestApp
+            {
+                [Module("Custom", RoutePrefix = "/api/custom")]
+                public class CustomModule : IModule
+                {
+                    public void ConfigureEndpoints(IEndpointRouteBuilder endpoints) { }
+                }
+            }
+
+            namespace TestApp.Endpoints
+            {
+                public class AutoEndpoint : IEndpoint
+                {
+                    public void Map(IEndpointRouteBuilder app)
+                    {
+                        app.MapGet("/auto", () => "auto");
+                    }
+                }
+            }
+            """;
+
+        var compilation = GeneratorTestHelper.CreateCompilation(source);
+        var result = GeneratorTestHelper.RunGenerator(compilation);
+
+        var endpointExt = GetGeneratedSource(result, "EndpointExtensions.g.cs");
+
+        // Should NOT auto-register endpoints because HasConfigureEndpoints is true
+        endpointExt.Should().NotContain("new global::TestApp.Endpoints.AutoEndpoint()");
+        // Should use the escape hatch instead
+        endpointExt.Should().Contain("ModuleExtensions.s_TestApp_CustomModule.ConfigureEndpoints(app);");
+    }
+
+    [Fact]
+    public void Module_WithConfigureEndpoints_UsesEscapeHatch()
+    {
+        var source = """
+            using Microsoft.AspNetCore.Builder;
+            using Microsoft.AspNetCore.Routing;
+            using SimpleModule.Core;
+
+            namespace TestApp
+            {
+                [Module("Manual")]
+                public class ManualModule : IModule
+                {
+                    public void ConfigureEndpoints(IEndpointRouteBuilder endpoints) { }
+                }
+            }
+            """;
+
+        var compilation = GeneratorTestHelper.CreateCompilation(source);
+        var result = GeneratorTestHelper.RunGenerator(compilation);
+
+        var endpointExt = GetGeneratedSource(result, "EndpointExtensions.g.cs");
+
+        endpointExt.Should().Contain("ModuleExtensions.s_TestApp_ManualModule.ConfigureEndpoints(app);");
+    }
+
+    [Fact]
+    public void Module_WithEndpointsAndConfigureEndpoints_SkipsAutoAndUsesEscapeHatch()
+    {
+        var source = """
+            using Microsoft.AspNetCore.Builder;
+            using Microsoft.AspNetCore.Routing;
+            using SimpleModule.Core;
+
+            namespace TestApp
+            {
+                [Module("Hybrid", RoutePrefix = "/api/hybrid")]
+                public class HybridModule : IModule
+                {
+                    public void ConfigureEndpoints(IEndpointRouteBuilder endpoints) { }
+                }
+            }
+
+            namespace TestApp.Endpoints
+            {
+                public class SomeEndpoint : IEndpoint
+                {
+                    public void Map(IEndpointRouteBuilder app)
+                    {
+                        app.MapGet("/some", () => "some");
+                    }
+                }
+            }
+            """;
+
+        var compilation = GeneratorTestHelper.CreateCompilation(source);
+        var result = GeneratorTestHelper.RunGenerator(compilation);
+
+        var endpointExt = GetGeneratedSource(result, "EndpointExtensions.g.cs");
+
+        // Auto-registration skipped
+        endpointExt.Should().NotContain("new global::TestApp.Endpoints.SomeEndpoint()");
+        // Escape hatch used
+        endpointExt.Should().Contain("ModuleExtensions.s_TestApp_HybridModule.ConfigureEndpoints(app);");
+    }
+
+    [Fact]
+    public void Module_WithNoEndpoints_NothingEmittedForThatModule()
+    {
+        var source = """
+            using SimpleModule.Core;
+
+            namespace TestApp
+            {
+                [Module("Empty")]
+                public class EmptyModule : IModule { }
+            }
+            """;
+
+        var compilation = GeneratorTestHelper.CreateCompilation(source);
+        var result = GeneratorTestHelper.RunGenerator(compilation);
+
+        var endpointExt = GetGeneratedSource(result, "EndpointExtensions.g.cs");
+
+        endpointExt.Should().NotContain("EmptyModule");
+        endpointExt.Should().NotContain("MapGroup");
+    }
+
+    [Fact]
+    public void MultipleModules_WithDifferentPrefixes_EachGetOwnGroup()
+    {
+        var source = """
+            using Microsoft.AspNetCore.Builder;
+            using Microsoft.AspNetCore.Routing;
+            using SimpleModule.Core;
+
+            namespace ModuleA
+            {
+                [Module("Alpha", RoutePrefix = "/api/alpha")]
+                public class AlphaModule : IModule { }
+            }
+
+            namespace ModuleA.Endpoints
+            {
+                public class AlphaEndpoint : IEndpoint
+                {
+                    public void Map(IEndpointRouteBuilder app)
+                    {
+                        app.MapGet("/", () => "alpha");
+                    }
+                }
+            }
+
+            namespace ModuleB
+            {
+                [Module("Beta", RoutePrefix = "/api/beta")]
+                public class BetaModule : IModule { }
+            }
+
+            namespace ModuleB.Endpoints
+            {
+                public class BetaEndpoint : IEndpoint
+                {
+                    public void Map(IEndpointRouteBuilder app)
+                    {
+                        app.MapGet("/", () => "beta");
+                    }
+                }
+            }
+            """;
+
+        var compilation = GeneratorTestHelper.CreateCompilation(source);
+        var result = GeneratorTestHelper.RunGenerator(compilation);
+
+        var endpointExt = GetGeneratedSource(result, "EndpointExtensions.g.cs");
+
+        endpointExt.Should().Contain("app.MapGroup(\"/api/alpha\").WithTags(\"Alpha\")");
+        endpointExt.Should().Contain("app.MapGroup(\"/api/beta\").WithTags(\"Beta\")");
+        endpointExt.Should().Contain("new global::ModuleA.Endpoints.AlphaEndpoint().Map(group)");
+        endpointExt.Should().Contain("new global::ModuleB.Endpoints.BetaEndpoint().Map(group)");
+    }
+
+    [Fact]
+    public void ViewEndpoints_WithViewPrefix_GetViewGroupWithExcludeFromDescription()
+    {
+        var source = """
+            using Microsoft.AspNetCore.Builder;
+            using Microsoft.AspNetCore.Routing;
+            using SimpleModule.Core;
+
+            namespace TestApp
+            {
+                [Module("Products", ViewPrefix = "/products")]
+                public class ProductsModule : IModule { }
+            }
+
+            namespace TestApp.Views
+            {
+                public class BrowseEndpoint : IViewEndpoint
+                {
+                    public void Map(IEndpointRouteBuilder app)
+                    {
+                        app.MapGet("/browse", () => "browse");
+                    }
+                }
+            }
+            """;
+
+        var compilation = GeneratorTestHelper.CreateCompilation(source);
+        var result = GeneratorTestHelper.RunGenerator(compilation);
+
+        var endpointExt = GetGeneratedSource(result, "EndpointExtensions.g.cs");
+
+        endpointExt.Should().Contain(
+            "var viewGroup = app.MapGroup(\"/products\").WithTags(\"Products\").ExcludeFromDescription();"
+        );
+        endpointExt.Should().Contain("new global::TestApp.Views.BrowseEndpoint().Map(viewGroup);");
+    }
+
+    [Fact]
+    public void ViewEndpoints_WithoutViewPrefix_MapDirectlyToApp()
+    {
+        var source = """
+            using Microsoft.AspNetCore.Builder;
+            using Microsoft.AspNetCore.Routing;
+            using SimpleModule.Core;
+
+            namespace TestApp
+            {
+                [Module("Test")]
+                public class TestModule : IModule { }
+            }
+
+            namespace TestApp.Views
+            {
+                public class HomeEndpoint : IViewEndpoint
+                {
+                    public void Map(IEndpointRouteBuilder app)
+                    {
+                        app.MapGet("/", () => "home");
+                    }
+                }
+            }
+            """;
+
+        var compilation = GeneratorTestHelper.CreateCompilation(source);
+        var result = GeneratorTestHelper.RunGenerator(compilation);
+
+        var endpointExt = GetGeneratedSource(result, "EndpointExtensions.g.cs");
+
+        endpointExt.Should().Contain("new global::TestApp.Views.HomeEndpoint().Map(app);");
+        endpointExt.Should().NotContain("viewGroup");
+    }
+
+    private static string GetGeneratedSource(Microsoft.CodeAnalysis.GeneratorDriverRunResult result, string fileName)
+    {
+        return result
+            .GeneratedTrees.First(t => t.FilePath.EndsWith(fileName, StringComparison.Ordinal))
+            .GetText()
+            .ToString();
+    }
+}
---
+++ tests/SimpleModule.Generator.Tests/TypeScriptDefinitionsEmitterTests.cs
@@ -0,0 +1,331 @@
+using FluentAssertions;
+using SimpleModule.Generator.Tests.Helpers;
+
+namespace SimpleModule.Generator.Tests;
+
+public class TypeScriptDefinitionsEmitterTests
+{
+    [Fact]
+    public void Dto_WithStringIntBool_MapsToCorrectTsTypes()
+    {
+        var source = """
+            using SimpleModule.Core;
+
+            namespace TestApp.Contracts;
+
+            [Module("TestApp")]
+            public class TestAppModule : IModule { }
+
+            [Dto]
+            public class ItemDto
+            {
+                public string Name { get; set; } = "";
+                public int Count { get; set; }
+                public bool IsActive { get; set; }
+            }
+            """;
+
+        var compilation = GeneratorTestHelper.CreateCompilation(source);
+        var result = GeneratorTestHelper.RunGenerator(compilation);
+
+        var tsSource = GetGeneratedSource(result, "DtoTypeScript_Contracts.g.cs");
+
+        tsSource.Should().Contain("name: string;");
+        tsSource.Should().Contain("count: number;");
+        tsSource.Should().Contain("isActive: boolean;");
+    }
+
+    [Fact]
+    public void Dto_WithDateTimeGuidDateOnly_MapsToString()
+    {
+        var source = """
+            using System;
+            using SimpleModule.Core;
+
+            namespace TestApp.Contracts;
+
+            [Module("TestApp")]
+            public class TestAppModule : IModule { }
+
+            [Dto]
+            public class DatesDto
+            {
+                public DateTime CreatedAt { get; set; }
+                public Guid Id { get; set; }
+                public DateOnly BirthDate { get; set; }
+            }
+            """;
+
+        var compilation = GeneratorTestHelper.CreateCompilation(source);
+        var result = GeneratorTestHelper.RunGenerator(compilation);
+
+        var tsSource = GetGeneratedSource(result, "DtoTypeScript_Contracts.g.cs");
+
+        tsSource.Should().Contain("createdAt: string;");
+        tsSource.Should().Contain("id: string;");
+        tsSource.Should().Contain("birthDate: string;");
+    }
+
+    [Fact]
+    public void Dto_WithListAndIEnumerable_MapsToArray()
+    {
+        var source = """
+            using System.Collections.Generic;
+            using SimpleModule.Core;
+
+            namespace TestApp.Contracts;
+
+            [Module("TestApp")]
+            public class TestAppModule : IModule { }
+
+            [Dto]
+            public class CollectionDto
+            {
+                public List<string> Tags { get; set; } = new();
+                public IEnumerable<int> Scores { get; set; } = new List<int>();
+            }
+            """;
+
+        var compilation = GeneratorTestHelper.CreateCompilation(source);
+        var result = GeneratorTestHelper.RunGenerator(compilation);
+
+        var tsSource = GetGeneratedSource(result, "DtoTypeScript_Contracts.g.cs");
+
+        tsSource.Should().Contain("tags: string[];");
+        tsSource.Should().Contain("scores: number[];");
+    }
+
+    [Fact]
+    public void Dto_WithNullableValueType_MapsToAny_DocumentsCurrentBehavior()
+    {
+        // Note: Nullable<T> via shorthand (int?) produces FQNs with nested global:: prefixes
+        // that the TypeMappingHelpers does not currently resolve. This test documents that behavior.
+        var source = """
+            using SimpleModule.Core;
+
+            namespace TestApp.Contracts;
+
+            [Module("TestApp")]
+            public class TestAppModule : IModule { }
+
+            [Dto]
+            public class NullableDto
+            {
+                public int? OptionalCount { get; set; }
+            }
+            """;
+
+        var compilation = GeneratorTestHelper.CreateCompilation(source);
+        var result = GeneratorTestHelper.RunGenerator(compilation);
+
+        var tsSource = GetGeneratedSource(result, "DtoTypeScript_Contracts.g.cs");
+
+        // Current behavior: nullable value types map to any due to FQN format limitations
+        tsSource.Should().Contain("optionalCount:");
+    }
+
+    [Fact]
+    public void Dto_ReferencingAnotherDto_UsesDtoInterfaceName()
+    {
+        var source = """
+            using SimpleModule.Core;
+
+            namespace TestApp.Contracts;
+
+            [Module("TestApp")]
+            public class TestAppModule : IModule { }
+
+            [Dto]
+            public class CategoryDto
+            {
+                public int Id { get; set; }
+                public string Name { get; set; } = "";
+            }
+
+            [Dto]
+            public class ProductDto
+            {
+                public int Id { get; set; }
+                public CategoryDto Category { get; set; } = new();
+            }
+            """;
+
+        var compilation = GeneratorTestHelper.CreateCompilation(source);
+        var result = GeneratorTestHelper.RunGenerator(compilation);
+
+        var tsSource = GetGeneratedSource(result, "DtoTypeScript_Contracts.g.cs");
+
+        tsSource.Should().Contain("category: CategoryDto;");
+        tsSource.Should().NotContain("category: any;");
+    }
+
+    [Fact]
+    public void PropertyNames_AreCamelCased()
+    {
+        var source = """
+            using SimpleModule.Core;
+
+            namespace TestApp.Contracts;
+
+            [Module("TestApp")]
+            public class TestAppModule : IModule { }
+
+            [Dto]
+            public class CamelDto
+            {
+                public string FirstName { get; set; } = "";
+                public int ProductId { get; set; }
+            }
+            """;
+
+        var compilation = GeneratorTestHelper.CreateCompilation(source);
+        var result = GeneratorTestHelper.RunGenerator(compilation);
+
+        var tsSource = GetGeneratedSource(result, "DtoTypeScript_Contracts.g.cs");
+
+        tsSource.Should().Contain("firstName: string;");
+        tsSource.Should().Contain("productId: number;");
+        tsSource.Should().NotContain("FirstName:");
+        tsSource.Should().NotContain("ProductId:");
+    }
+
+    [Fact]
+    public void File_UsesSimpleModuleTsWrapper()
+    {
+        var source = """
+            using SimpleModule.Core;
+
+            namespace TestApp.Contracts;
+
+            [Module("TestApp")]
+            public class TestAppModule : IModule { }
+
+            [Dto]
+            public class SimpleDto
+            {
+                public int Id { get; set; }
+            }
+            """;
+
+        var compilation = GeneratorTestHelper.CreateCompilation(source);
+        var result = GeneratorTestHelper.RunGenerator(compilation);
+
+        var tsSource = GetGeneratedSource(result, "DtoTypeScript_Contracts.g.cs");
+
+        tsSource.Should().Contain("#if SIMPLEMODULE_TS");
+        tsSource.Should().Contain("/*");
+        tsSource.Should().Contain("*/");
+        tsSource.Should().Contain("#endif");
+    }
+
+    [Fact]
+    public void File_IncludesModuleComment()
+    {
+        var source = """
+            using SimpleModule.Core;
+
+            namespace TestApp.Contracts;
+
+            [Module("TestApp")]
+            public class TestAppModule : IModule { }
+
+            [Dto]
+            public class SimpleDto
+            {
+                public int Id { get; set; }
+            }
+            """;
+
+        var compilation = GeneratorTestHelper.CreateCompilation(source);
+        var result = GeneratorTestHelper.RunGenerator(compilation);
+
+        var tsSource = GetGeneratedSource(result, "DtoTypeScript_Contracts.g.cs");
+
+        tsSource.Should().Contain("// @module Contracts");
+    }
+
+    [Fact]
+    public void NoDtoTypes_NoTypeScriptFileEmitted()
+    {
+        var source = """
+            using SimpleModule.Core;
+
+            namespace TestApp;
+
+            [Module("Test")]
+            public class TestModule : IModule { }
+            """;
+
+        var compilation = GeneratorTestHelper.CreateCompilation(source);
+        var result = GeneratorTestHelper.RunGenerator(compilation);
+
+        result
+            .GeneratedTrees.Should()
+            .NotContain(t => t.FilePath.Contains("DtoTypeScript_"));
+    }
+
+    [Fact]
+    public void Dto_WithUnknownType_MapsToAny()
+    {
+        var source = """
+            using SimpleModule.Core;
+
+            namespace TestApp.Contracts;
+
+            [Module("TestApp")]
+            public class TestAppModule : IModule { }
+
+            public class NonDtoClass
+            {
+                public int Value { get; set; }
+            }
+
+            [Dto]
+            public class ContainerDto
+            {
+                public NonDtoClass Child { get; set; } = new();
+            }
+            """;
+
+        var compilation = GeneratorTestHelper.CreateCompilation(source);
+        var result = GeneratorTestHelper.RunGenerator(compilation);
+
+        var tsSource = GetGeneratedSource(result, "DtoTypeScript_Contracts.g.cs");
+
+        tsSource.Should().Contain("child: any;");
+    }
+
+    [Fact]
+    public void Dto_GeneratesExportInterface()
+    {
+        var source = """
+            using SimpleModule.Core;
+
+            namespace TestApp.Contracts;
+
+            [Module("TestApp")]
+            public class TestAppModule : IModule { }
+
+            [Dto]
+            public class MyDto
+            {
+                public int Id { get; set; }
+            }
+            """;
+
+        var compilation = GeneratorTestHelper.CreateCompilation(source);
+        var result = GeneratorTestHelper.RunGenerator(compilation);
+
+        var tsSource = GetGeneratedSource(result, "DtoTypeScript_Contracts.g.cs");
+
+        tsSource.Should().Contain("export interface MyDto {");
+    }
+
+    private static string GetGeneratedSource(Microsoft.CodeAnalysis.GeneratorDriverRunResult result, string fileName)
+    {
+        return result
+            .GeneratedTrees.First(t => t.FilePath.EndsWith(fileName, StringComparison.Ordinal))
+            .GetText()
+            .ToString();
+    }
+}
