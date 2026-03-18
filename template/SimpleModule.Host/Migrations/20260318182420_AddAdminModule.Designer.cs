43244d9914a58ae201b9d6ab2af0615c9e77d9df
Anto Subash <antosubash@live.com>
2026-03-18 19:24:36 +0100

    feat(admin): add EF Core migration for Admin module tables
    Adds AuditLogEntries (admin schema), RolePermissions, UserPermissions
    (users schema), and DeactivatedAt column on AspNetUsers.

--- template/SimpleModule.Host/Migrations/20260318182420_AddAdminModule.Designer.cs
+++ template/SimpleModule.Host/Migrations/20260318182420_AddAdminModule.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using SimpleModule.Host;
@@ -11,9 +12,11 @@ using SimpleModule.Host;
 namespace SimpleModule.Host.Migrations
 {
     [DbContext(typeof(HostDbContext))]
-    partial class HostDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260318182420_AddAdminModule")]
+    partial class AddAdminModule
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
