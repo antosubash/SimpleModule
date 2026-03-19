abc92cd3aa19d1f9c2492628b4b925f5be5ff7be
Anto Subash <antosubash@live.com>
2026-03-19 11:29:11 +0100

    feat(openiddict): add OpenIddict.Contracts with auth constants

---
+++ modules/OpenIddict/src/OpenIddict.Contracts/ClientConstants.cs
@@ -0,0 +1,11 @@
+namespace SimpleModule.OpenIddict.Contracts;
+
+public static class ClientConstants
+{
+    public const string ClientId = "simplemodule-client";
+    public const string ClientDisplayName = "SimpleModule Client";
+    public const string SwaggerCallbackPath = "/swagger/oauth2-redirect.html";
+    public const string OAuthCallbackPath = "/oauth-callback";
+    public const string PostLogoutRedirectPath = "/";
+    public const string DefaultBaseUrl = "https://localhost:5001";
+}
---
+++ modules/OpenIddict/src/OpenIddict.Contracts/ConnectRouteConstants.cs
@@ -0,0 +1,9 @@
+namespace SimpleModule.OpenIddict.Contracts;
+
+public static class ConnectRouteConstants
+{
+    public const string ConnectAuthorize = "/connect/authorize";
+    public const string ConnectToken = "/connect/token";
+    public const string ConnectEndSession = "/connect/endsession";
+    public const string ConnectUserInfo = "/connect/userinfo";
+}
