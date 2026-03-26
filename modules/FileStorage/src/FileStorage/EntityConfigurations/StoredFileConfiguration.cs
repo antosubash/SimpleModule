f7819d8bc647c63131fbc20ea36b0bb04990822e
Anto Subash <antosubash@live.com>
2026-03-26 16:12:14 +0100

    feat: add FileStorage module infrastructure

---
+++ modules/FileStorage/src/FileStorage/EntityConfigurations/StoredFileConfiguration.cs
@@ -0,0 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SimpleModule.FileStorage.Contracts;
+
+namespace SimpleModule.FileStorage.EntityConfigurations;
+
+public class StoredFileConfiguration : IEntityTypeConfiguration<StoredFile>
+{
+    public void Configure(EntityTypeBuilder<StoredFile> builder)
+    {
+        builder.HasKey(f => f.Id);
+        builder.Property(f => f.Id).ValueGeneratedOnAdd();
+        builder.Property(f => f.FileName).IsRequired().HasMaxLength(512);
+        builder.Property(f => f.StoragePath).IsRequired().HasMaxLength(1024);
+        builder.Property(f => f.ContentType).IsRequired().HasMaxLength(256);
+        builder.Property(f => f.Folder).HasMaxLength(1024);
+        builder.HasIndex(f => f.Folder);
+    }
+}
