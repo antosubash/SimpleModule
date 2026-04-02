e65b3a2800596bcd304643645838e12fa9059ac4
Anto Subash <antosubash@live.com>
2026-04-02 19:57:40 +0200

    test(localization): add integration test for Inertia translation props

---
+++ modules/Localization/tests/SimpleModule.Localization.Tests/Integration/InertiaTranslationPropsTests.cs
@@ -0,0 +1,69 @@
+using System.Net.Http.Json;
+using System.Text.Json;
+using FluentAssertions;
+using SimpleModule.Core.Inertia;
+using SimpleModule.Tests.Shared.Fixtures;
+
+namespace SimpleModule.Localization.Tests.Integration;
+
+public class InertiaTranslationPropsTests : IClassFixture<SimpleModuleWebApplicationFactory>
+{
+    private readonly SimpleModuleWebApplicationFactory _factory;
+
+    public InertiaTranslationPropsTests(SimpleModuleWebApplicationFactory factory)
+    {
+        _factory = factory;
+    }
+
+    [Fact]
+    public async Task Inertia_Response_Contains_Locale_And_Translations()
+    {
+        var client = _factory.CreateClient();
+        client.DefaultRequestHeaders.Add("X-Inertia", "true");
+        client.DefaultRequestHeaders.Add("X-Inertia-Version", InertiaMiddleware.Version);
+
+        var response = await client.GetAsync("/products/browse");
+        response.EnsureSuccessStatusCode();
+
+        var json = await response.Content.ReadFromJsonAsync<JsonElement>();
+        var props = json.GetProperty("props");
+
+        props.TryGetProperty("locale", out var locale).Should().BeTrue("Inertia response should contain locale shared prop");
+        locale.GetString().Should().NotBeNullOrEmpty();
+
+        props.TryGetProperty("translations", out _).Should().BeTrue("Inertia response should contain translations shared prop");
+    }
+
+    [Fact]
+    public async Task Locale_Defaults_To_En_When_No_AcceptLanguage()
+    {
+        var client = _factory.CreateClient();
+        client.DefaultRequestHeaders.Add("X-Inertia", "true");
+        client.DefaultRequestHeaders.Add("X-Inertia-Version", InertiaMiddleware.Version);
+
+        var response = await client.GetAsync("/products/browse");
+        response.EnsureSuccessStatusCode();
+
+        var json = await response.Content.ReadFromJsonAsync<JsonElement>();
+        var props = json.GetProperty("props");
+
+        props.GetProperty("locale").GetString().Should().Be("en");
+    }
+
+    [Fact]
+    public async Task Locale_Respects_AcceptLanguage_Header()
+    {
+        var client = _factory.CreateClient();
+        client.DefaultRequestHeaders.Add("X-Inertia", "true");
+        client.DefaultRequestHeaders.Add("X-Inertia-Version", InertiaMiddleware.Version);
+        client.DefaultRequestHeaders.Add("Accept-Language", "fr-FR,fr;q=0.9,en;q=0.8");
+
+        var response = await client.GetAsync("/products/browse");
+        response.EnsureSuccessStatusCode();
+
+        var json = await response.Content.ReadFromJsonAsync<JsonElement>();
+        var props = json.GetProperty("props");
+
+        props.GetProperty("locale").GetString().Should().Be("fr");
+    }
+}
