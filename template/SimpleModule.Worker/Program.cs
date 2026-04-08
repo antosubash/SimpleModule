d8913b003ef8562d7933b0481e4d3dec5211444e
Anto Subash <antosubash@live.com>
2026-04-08 21:50:44 +0200

    removed random table create

--- template/SimpleModule.Worker/Program.cs
+++ template/SimpleModule.Worker/Program.cs
@@ -1,4 +1,4 @@
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Storage;
 using Microsoft.Extensions.DependencyInjection;
@@ -30,32 +30,5 @@ builder.Services.CollectModuleSettings();
 
 // Register module agent definitions.
 builder.Services.AddModuleAgents();
-
 var app = builder.Build();
-
-// Ensure all module database schemas exist before hosted services start.
-// This mirrors UseSimpleModuleInfrastructure in the web host.
-using (var scope = app.Services.CreateScope())
-{
-    var infos = scope.ServiceProvider.GetServices<ModuleDbContextInfo>();
-    foreach (var info in infos)
-    {
-        if (scope.ServiceProvider.GetService(info.DbContextType) is not DbContext db)
-            continue;
-
-        // EnsureCreated returns true only if the DB was just created.
-        // When multiple contexts share a SQLite file, only the first call creates
-        // the file; subsequent calls return false without creating their tables.
-        // We use EnsureCreated then fall back to CreateTablesAsync() for subsequent contexts.
-        if (!await db.Database.EnsureCreatedAsync())
-        {
-            var creator = db.GetService<IRelationalDatabaseCreator>();
-            try { await creator.CreateTablesAsync(); }
-#pragma warning disable CA1031
-            catch { /* tables already exist */ }
-#pragma warning restore CA1031
-        }
-    }
-}
-
 await app.RunAsync();
