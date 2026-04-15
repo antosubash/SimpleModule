c7a690b3d53384f75ec01feae389ce4b529df361
Anto Subash <antosubash@live.com>
2026-04-15 20:37:19 +0200

    refactor(generator): extract DiscoveryDataBuilder for final conversion block

--- framework/SimpleModule.Generator/Discovery/SymbolDiscovery.cs
+++ framework/SimpleModule.Generator/Discovery/SymbolDiscovery.cs
@@ -1,7 +1,5 @@
 using System;
 using System.Collections.Generic;
-using System.Collections.Immutable;
-using System.Linq;
 using System.Threading;
 using Microsoft.CodeAnalysis;
 
@@ -201,146 +199,24 @@ internal static class SymbolDiscovery
             illegalReferences
         );
 
-        return new DiscoveryData(
-            modules
-                .Select(m => new ModuleInfoRecord(
-                    m.FullyQualifiedName,
-                    m.ModuleName,
-                    m.AssemblyName,
-                    m.HasConfigureServices,
-                    m.HasConfigureEndpoints,
-                    m.HasConfigureMenu,
-                    m.HasConfigurePermissions,
-                    m.HasConfigureMiddleware,
-                    m.HasConfigureSettings,
-                    m.HasConfigureFeatureFlags,
-                    m.HasConfigureAgents,
-                    m.HasConfigureRateLimits,
-                    m.RoutePrefix,
-                    m.ViewPrefix,
-                    m.Endpoints.Select(e => new EndpointInfoRecord(
-                            e.FullyQualifiedName,
-                            e.RequiredPermissions.ToImmutableArray(),
-                            e.AllowAnonymous,
-                            e.RouteTemplate,
-                            e.HttpMethod
-                        ))
-                        .ToImmutableArray(),
-                    m.Views.Select(v => new ViewInfoRecord(
-                            v.FullyQualifiedName,
-                            v.Page ?? "",
-                            v.RouteTemplate,
-                            v.Location
-                        ))
-                        .ToImmutableArray(),
-                    m.Location
-                ))
-                .ToImmutableArray(),
-            dtoTypes
-                .Select(d => new DtoTypeInfoRecord(
-                    d.FullyQualifiedName,
-                    d.SafeName,
-                    d.BaseTypeFqn,
-                    d.Properties.Select(p => new DtoPropertyInfoRecord(
-                            p.Name,
-                            p.TypeFqn,
-                            p.UnderlyingTypeFqn,
-                            p.HasSetter
-                        ))
-                        .ToImmutableArray()
-                ))
-                .ToImmutableArray(),
-            dbContexts
-                .Select(c => new DbContextInfoRecord(
-                    c.FullyQualifiedName,
-                    c.ModuleName,
-                    c.IsIdentityDbContext,
-                    c.IdentityUserTypeFqn,
-                    c.IdentityRoleTypeFqn,
-                    c.IdentityKeyTypeFqn,
-                    c.DbSets.Select(d => new DbSetInfoRecord(
-                            d.PropertyName,
-                            d.EntityFqn,
-                            d.EntityAssemblyName,
-                            d.EntityLocation
-                        ))
-                        .ToImmutableArray(),
-                    c.Location
-                ))
-                .ToImmutableArray(),
-            entityConfigs
-                .Select(e => new EntityConfigInfoRecord(
-                    e.ConfigFqn,
-                    e.EntityFqn,
-                    e.ModuleName,
-                    e.Location
-                ))
-                .ToImmutableArray(),
-            dependencies.ToImmutableArray(),
-            illegalReferences.ToImmutableArray(),
-            contractInterfaces.ToImmutableArray(),
-            contractImplementations
-                .Select(c => new ContractImplementationRecord(
-                    c.InterfaceFqn,
-                    c.ImplementationFqn,
-                    c.ModuleName,
-                    c.IsPublic,
-                    c.IsAbstract,
-                    c.DependsOnDbContext,
-                    c.Location,
-                    c.Lifetime
-                ))
-                .ToImmutableArray(),
-            permissionClasses
-                .Select(p => new PermissionClassRecord(
-                    p.FullyQualifiedName,
-                    p.ModuleName,
-                    p.IsSealed,
-                    p.Fields.Select(f => new PermissionFieldRecord(
-                            f.FieldName,
-                            f.Value,
-                            f.IsConstString,
-                            f.Location
-                        ))
-                        .ToImmutableArray(),
-                    p.Location
-                ))
-                .ToImmutableArray(),
-            featureClasses
-                .Select(f => new FeatureClassRecord(
-                    f.FullyQualifiedName,
-                    f.ModuleName,
-                    f.IsSealed,
-                    f.Fields.Select(ff => new FeatureFieldRecord(
-                            ff.FieldName,
-                            ff.Value,
-                            ff.IsConstString,
-                            ff.Location
-                        ))
-                        .ToImmutableArray(),
-                    f.Location
-                ))
-                .ToImmutableArray(),
-            interceptors
-                .Select(i => new InterceptorInfoRecord(
-                    i.FullyQualifiedName,
-                    i.ModuleName,
-                    i.ConstructorParamTypeFqns.ToImmutableArray(),
-                    i.Location
-                ))
-                .ToImmutableArray(),
-            vogenValueObjects.ToImmutableArray(),
-            moduleOptionsList.ToImmutableArray(),
-            agentDefinitions
-                .Select(a => new AgentDefinitionRecord(a.FullyQualifiedName, a.ModuleName))
-                .ToImmutableArray(),
-            agentToolProviders
-                .Select(a => new AgentToolProviderRecord(a.FullyQualifiedName, a.ModuleName))
-                .ToImmutableArray(),
-            knowledgeSources
-                .Select(k => new KnowledgeSourceRecord(k.FullyQualifiedName, k.ModuleName))
-                .ToImmutableArray(),
-            contractsAssemblyMap.Keys.ToImmutableArray(),
+        return DiscoveryDataBuilder.Build(
+            modules,
+            dtoTypes,
+            dbContexts,
+            entityConfigs,
+            dependencies,
+            illegalReferences,
+            contractInterfaces,
+            contractImplementations,
+            permissionClasses,
+            featureClasses,
+            interceptors,
+            vogenValueObjects,
+            moduleOptionsList,
+            agentDefinitions,
+            agentToolProviders,
+            knowledgeSources,
+            contractsAssemblyMap,
             s.HasAgentsAssembly,
             hostAssemblyName
         );
